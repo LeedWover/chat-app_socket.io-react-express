22285f2efe7fb4897ae40c9769a7e0afa6d9a5a8
LeedWover <aresone.gaben@gmail.com>
2020-03-27 19:14:16 +0100

    _

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
-var cors = require('cors');
+const cors = require('cors');
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, { cookie: false });
 
 app.use(cors());
 
